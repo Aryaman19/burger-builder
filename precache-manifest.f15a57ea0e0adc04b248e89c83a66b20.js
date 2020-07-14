7ff5349355041aad9865d70406cc9d8dafbd381f
Aryaman19 <aryaman.thakur999888@gmail.com>
2020-07-14 16:13:36 +0530

    Updates

---
+++ precache-manifest.f15a57ea0e0adc04b248e89c83a66b20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded45ed155f7ed156c1a0821b26d83a0",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "6beaf49fb7e3e75dc3d7",
+    "url": "/burger-builder/static/css/main.b6fbde40.chunk.css"
+  },
+  {
+    "revision": "9c663fbe0b132fb16945",
+    "url": "/burger-builder/static/js/2.f0499692.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.f0499692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6beaf49fb7e3e75dc3d7",
+    "url": "/burger-builder/static/js/main.1a4cff39.chunk.js"
+  },
+  {
+    "revision": "3743a9caf0853cc6f96a",
+    "url": "/burger-builder/static/js/runtime-main.f3cd13ce.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
